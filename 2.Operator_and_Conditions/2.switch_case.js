c95eac6a6b5f305c57f73eb2fde7f22b17ff5649
Prasad Adhikary <162437568+Prasad-1004@users.noreply.github.com>
2024-05-31 07:07:18 +0000

    cd

---
+++ 2.Operator_and_Conditions/2.switch_case.js
@@ -0,0 +1,28 @@
+const prompt = require("prompt-sync")()
+
+let a = prompt("What is the day :")
+
+switch(a){
+    case '1':
+        console.log("It is Monday.")
+        break;
+    case '2':
+        console.log("It is Tuesday.")
+        break;
+    case '3':
+        console.log("It is Wednesday.")
+        break;
+    case '4':
+        console.log("It is Thursday.")
+        break;
+    case '5':
+        console.log("It is Friday.")
+        break;
+    case '6':
+        console.log("It is Saturday.")
+        break;
+    case '7':
+        console.log("It is Sunday.")
+        break;
+                                                                                             
+}
